fb0ab4e5e8723a75cf2e3adab13b79a76fc135a1
Thomas Mapesa <mapesa@gmail.com>
2014-05-05 07:33:47 +0300

    Models for linking tables - perhaps these should be replaced with DB calls or fleshing out the models that use them.

---
+++ app/models/TestTypeSpecimenType.php
@@ -0,0 +1,12 @@
+<?php
+
+class TestTypeSpecimenType extends Eloquent
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'testtype_specimentype';
+
+}
