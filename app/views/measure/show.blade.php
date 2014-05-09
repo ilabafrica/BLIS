3ded5d41d635852e35af12a7a7d16de61bcc3e9c
ereng <aereng@gmail.com>
2014-05-09 17:19:46 +0300

    Formatted display layout for measures

--- app/views/measure/show.blade.php
+++ app/views/measure/show.blade.php
@@ -17,21 +17,34 @@
 			</div>
 		</div>
 		<div class="panel-body">
-			<div class="display-details">
-				<h3><strong>Name:</strong>{{ $measure->name }} </h3>
-				<p><strong>Description:</strong>{{ $measure->description }}</p>
-				<p><strong>Measure Type:</strong>{{ $measure->measureType->name }}</p>
-				<p><strong>Measure Range:</strong>{{ $measure->measure_range }}</p>
-				<p><ul>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="col-md-6"><strong>Name:</strong></div><div class="col-md-6">{{ $measure->name }}</div>
+					<div class="col-md-6"><strong>Description:</strong></div><div class="col-md-6">{{ $measure->description }}</div>
+					<div class="col-md-6"><strong>Measure Type:</strong></div><div class="col-md-6">{{ $measure->measureType->name }}</div>
+					<?php if ($measure->measureType->id == 2) { ?>
+					<div class="col-md-6">Measure Range:</div>{{ $measure->measure_range }}<div class="col-md-6"></div>
+					<?php } ?>
+				</div>
+				<div class="col-md-6">
+				</div>
+				<?php if ($measure->measureType->id == 1) { ?>
+
+				<div class="col-md-12">
+				<br>
+				<br>
 					<?php $gender = ['Male', 'Female', 'Both']; ?>
+					<div class="col-md-4"><strong>Age Range</strong></div><div class="col-md-4"><strong>Gender</strong></div><div class="col-md-4"><strong>Measure Range</strong></div>
+					<div class="col-md-2">Min</div><div class="col-md-2">Max</div><div class="col-md-4">Gender</div><div class="col-md-2">Lower Limit</div><div class="col-md-2">Upper Limit</div>
 					@foreach($measure->measureRanges as $range)
-					<li>Minimum Age: {{ $range->age_min }}</li>
-					<li>Maximum Age: {{ $range->age_max }}</li>
-					<li>Gender: {{ $gender[$range->sex - 1] }}</li>
-					<li>Upper Limit: {{ $range->range_upper }}</li>
-					<li>Lower Limit: {{ $range->range_lower }}</li>
+					<div class="col-md-2">{{ $range->age_min }}</div>
+					<div class="col-md-2">{{ $range->age_max }}</div>
+					<div class="col-md-4">{{ $gender[$range->sex - 1] }}</div>
+					<div class="col-md-2">{{ $range->range_lower }}</div>
+					<div class="col-md-2">{{ $range->range_upper }}</div>
 					@endforeach
-				</ul></p>
-			</div>
+				</div>
+				<?php } ?>
+			</div>			
 		</div>
 	</div>
