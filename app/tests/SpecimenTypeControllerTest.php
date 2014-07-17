a42410c1239e0331376856949a8d3048988ce81d
ereng <aereng@gmail.com>
2014-07-17 16:06:03 +0300

    Added Controller Tests
    Added tests for MeasureController, PatientController,
    SpecimenTypeController, TestCategoryController and UserController
    Removed ExampleTest

---
+++ app/tests/SpecimenTypeControllerTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Tests the SpecimenTypeController functions that store, edit and delete specimenTypes 
+ * @author  (c) @iLabAfrica, Emmanuel Kitsao, Brian Kiprop, Thomas Mapesa, Anthony Ereng
+ */
+class SpecimenTypeControllerTest extends TestCase 
+{
+	/**
+	 * Contains the testing sample data for the SpecimenTypeController.
+	 *
+	 * @return void
+	 */
+    public function __construct()
+    {
+    	// Initial sample storage data
+		$this->input = array(
+			'name' => 'SynovialFlud',
+			'description' => 'Lets see!',
+		);
+
+		// Edition sample data
+		$this->inputUpdate = array(
+			'name' => 'SynovialFluid',
+			'description' => 'Honestly have no idea',
+		);
+
+		$this->testSpecimenTypeId = NULL;
+    }
+	
+	/**
+	 * Tests the store funtion in the SpecimenTypeController
+	 * @param  testing Sample from the constructor
+	 * @return int $testSpecimenTypeId ID of SpecimenType stored; used in testUpdate() to identify test for update
+	 */    
+ 	public function testStore() 
+  	{
+		echo "\n\nSPECIMEN TYPE CONTROLLER TEST\n\n";
+  		 // Store the SpecimenTypes
+		$this->runStore($this->input);
+
+		$specimenTypesSaved = SpecimenType::orderBy('id','desc')->take(1)->get();
+		foreach ($specimenTypesSaved as $specimenTypeSaved) {
+			$this->testSpecimenTypeId = $specimenTypeSaved->id;
+			$this->assertEquals($specimenTypeSaved->name , $this->input['name']);
+			$this->assertEquals($specimenTypeSaved->description ,$this->input['description']);
+			$testSpecimenTypeId = $this->testSpecimenTypeId;
+		}
+		echo "SpecimenType created\n";
+		return $testSpecimenTypeId;
+  	}
+
+  	/**
+  	 * Tests the update funtion in the SpecimenTypeController
+     * @depends testStore
+	 * @param  int $testSpecimenTypeId SpecimenType ID from testStore(), testing Sample from the constructor
+	 * @return void
+     */
+	public function testUpdate($testSpecimenTypeId)
+	{
+		// Update the SpecimenTypes
+		$this->runUpdate($this->inputUpdate, $testSpecimenTypeId);
+
+		$specimenTypesSaved = SpecimenType::orderBy('id','desc')->take(1)->get();
+		foreach ($specimenTypesSaved as $specimenTypeSaved) {
+			$this->assertEquals($specimenTypeSaved->name , $this->inputUpdate['name']);
+			$this->assertEquals($specimenTypeSaved->description ,$this->inputUpdate['description']);
+		}
+		echo "\nSpecimenType updated\n";
+	}
+
+	/**
+  	 * Tests the update funtion in the SpecimenTypeController
+     * @depends testStore
+	 * @param  int $testSpecimenTypeId SpecimenType ID from testStore()
+	 * @return void
+     */
+	public function testDelete($testSpecimenTypeId)
+	{
+		$this->runDelete($testSpecimenTypeId);
+		$specimenTypesSaved = SpecimenType::withTrashed()->orderBy('id','desc')->take(1)->get();
+		foreach ($specimenTypesSaved as $specimenTypeSaved) {
+			$this->assertNotNull($specimenTypeSaved->deleted_at);
+	    }
+		echo "\nSpecimenType softDelete\n";
+	    $this->removeTestData($testSpecimenTypeId);
+		echo "sample SpecimenType removed from the Database\n";
+	}
+	
+	
+  	/**
+  	 *Executes the store funtion in the SpecimenTypeController
+  	 * @param  array $input SpecimenType details
+	 * @return void
+  	 */
+	public function runStore($input)
+	{
+		Input::replace($input);
+    	$specimenType = new SpecimenTypeController;
+    	$specimenType->store();
+	}
+
+  	/**
+  	 * Executes the update funtion in the SpecimenTypeController
+  	 * @param  array $input SpecimenType details, int $id ID of the SpecimenType stored
+	 * @return void
+  	 */
+	public function runUpdate($input, $id)
+	{
+		Input::replace($input);
+    	$specimenType = new SpecimenTypeController;
+    	$specimenType->update($id);
+	}
+
+	/**
+	 * Executes the delete funtion in the SpecimenTypeController
+	 * @param  int $id ID of SpecimenType stored
+	 * @return void
+	 */
+	public function runDelete($id)
+	{
+		$specimenType = new SpecimenTypeController;
+    	$specimenType->delete($id);
+	}
+
+	 /**
+	  * Force delete all sample SpecimenTypes from the database
+	  * @param  int $id SpecimenType ID
+	  * @return void
+	  */
+	public function removeTestData($id)
+	{
+		DB::table('specimen_type')->delete($id);
+	}
+}
---
+++ app/tests/TestCategoryControllerTest.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Tests the TestCategoryController functions that store, edit and delete testCategorys 
+ * @author  (c) @iLabAfrica, Emmanuel Kitsao, Brian Kiprop, Thomas Mapesa, Anthony Ereng
+ */
+class TestCategoryControllerTest extends TestCase 
+{
+	/**
+	 * Contains the testing sample data for the TestCategoryController.
+	 *
+	 * @return void
+	 */
+    public function __construct()
+    {
+    	// Initial sample storage data
+		$this->input = array(
+			'name' => 'Parasitlogy',
+			'description' => 'Lets see',
+		);
+
+		
+		// Edition sample data
+		$this->inputUpdate = array(
+			'name' => 'Parasitology',
+			'description' => 'I honestly have no idea',
+		);
+
+	
+		$this->testTestCategoryId = NULL;
+    }
+	
+	/**
+	 * Tests the store function in the TestCategoryController
+	 * @param  testing Sample from the constructor
+	 * @return int $testTestCategoryId ID of TestCategory stored; used in testUpdate() to identify test for update
+	 */    
+ 	public function testStore() 
+  	{
+		echo "\n\nTEST CATEGORY CONTROLLER TEST\n\n";
+  		 // Store the TestCategory
+		$this->runStore($this->input);
+
+		$testCategoriesSaved = TestCategory::orderBy('id','desc')->take(1)->get();
+		foreach ($testCategoriesSaved as $testCategorySaved) {
+			$this->testTestCategoryId = $testCategorySaved->id;
+			$this->assertEquals($testCategorySaved->name , $this->input['name']);
+			$this->assertEquals($testCategorySaved->description ,$this->input['description']);
+			$testTestCategoryId = $this->testTestCategoryId;
+		}
+		echo "TestCategory created\n";
+		return $testTestCategoryId;
+  	}
+
+  	/**
+  	 * Tests the update function in the TestCategoryController
+     * @depends testStore
+	 * @param  int $testTestCategoryId TestCategory ID from testStore(), testing Sample from the constructor
+	 * @return void
+     */
+	public function testUpdate($testTestCategoryId)
+	{
+		// Update the TestCategory
+		$this->runUpdate($this->inputUpdate, $testTestCategoryId);
+
+		$testCategoriesSaved = TestCategory::orderBy('id','desc')->take(1)->get();
+		foreach ($testCategoriesSaved as $testCategorySaved) {
+			$this->testTestCategoryId = $testCategorySaved->id;
+			$this->assertEquals($testCategorySaved->name , $this->inputUpdate['name']);
+			$this->assertEquals($testCategorySaved->description ,$this->inputUpdate['description']);
+		}
+		echo "\nTestCategory updated\n";
+	}
+
+	
+	
+	/**
+  	 * Tests the update function in the TestCategoryController
+     * @depends testStore
+	 * @param  int $testTestCategoryId TestCategory IDs from testStore()
+	 * @return void
+     */
+	public function testDelete($testTestCategoryId)
+	{
+		$this->runDelete($testTestCategoryId);
+		$testCategoriesSaved = TestCategory::withTrashed()->orderBy('id','desc')->take(1)->get();
+		foreach ($testCategoriesSaved as $testCategorySaved) {
+			$this->assertNotNull($testCategorySaved->deleted_at);
+		}
+		echo "\nTestCategory softDeleted\n";
+	    $this->removeTestData($testTestCategoryId);
+		echo "sample TestCategory removed from the Database\n";
+	}
+	
+	
+  	/**
+  	 *Executes the store function in the TestCategoryController
+  	 * @param  int $input TestCategory details
+	 * @return void
+  	 */
+	public function runStore($input)
+	{
+		Input::replace($input);
+    	$testCategory = new TestCategoryController;
+    	$testCategory->store();
+	}
+
+  	/**
+  	 * Executes the update function in the TestCategoryController
+  	 * @param  array $input TestCategory details, int $id ID of the TestCategory stored
+	 * @return void
+  	 */
+	public function runUpdate($input, $id)
+	{
+		Input::replace($input);
+    	$testCategory = new TestCategoryController;
+    	$testCategory->update($id);
+	}
+
+	/**
+	 * Executes the delete function in the TestCategoryController
+	 * @param  int $id ID of TestCategory stored
+	 * @return void
+	 */
+	public function runDelete($id)
+	{
+		$testCategory = new TestCategoryController;
+    	$testCategory->delete($id);
+	}
+
+	 /**
+	  * Force delete all sample TestCategory from the database
+	  * @param  int $id TestCategory ID
+	  * @return void
+	  */
+	public function removeTestData($id)
+	{
+		DB::table('test_category')->delete($id);
+	}
+}
