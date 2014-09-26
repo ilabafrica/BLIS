58a4ff4fa8d8adbed001025c97c92c47a0df74f9
Kitsao Emmanuel <kittemm@gmail.com>
2014-09-26 20:13:48 +0300

    sample chart with js

--- app/views/reports/prevalence/index.blade.php
+++ app/views/reports/prevalence/index.blade.php
@@ -56,7 +56,7 @@
 				{{ HTML::ul($errors->all()) }}
 			</div>
 		@endif
-  <!-- <div id="chartdiv"></div> -->
+  <div id="chartContainer"></div>
   <div id="grid">
   	<div class="table-responsive">
 	  <table class="table table-striped">
@@ -91,12 +91,193 @@
 
 </div>
 <!-- Begin FusionCharts scripts -->
-{{ HTML::script('FusionCharts/JSClass/FusionCharts.js') }}
+{{ HTML::script('fusioncharts/js/fusioncharts.js') }}
+{{ HTML::script('fusioncharts/js/themes/fusioncharts.theme.ocean.js') }}
 <!-- End fusioncharts scripts -->
 <script type="text/javascript">
-   var chart = new FusionCharts("FusionCharts/Charts/MSLine.swf", "ChartId", "980", "550", "0", "0");
-   chart.setDataURL("fusion_data/prevalence_rates.php");		   
-   chart.render("chartdiv");
+	FusionCharts.ready(function(){
+	    var revenueChart = new FusionCharts({
+	      type: "msline",
+	      renderAt: "chartContainer",
+	      width: "980",
+	      height: "500",
+	      dataFormat: "json",
+	      dataSource: {
+	       "chart": {
+	        "caption": "Daily Visits",
+	        "subcaption": "(from 8/6/2013 to 8/12/2013)",
+	        "linethickness": "1",
+	        "showvalues": "0",
+	        "formatnumberscale": "0",
+	        "anchorradius": "2",
+	        "divlinecolor": "666666",
+	        "divlinealpha": "30",
+	        "divlineisdashed": "1",
+	        "labelstep": "2",
+	        "bgcolor": "FFFFFF",
+	        "showalternatehgridcolor": "0",
+	        "labelpadding": "10",
+	        "canvasborderthickness": "1",
+	        "legendiconscale": "1.5",
+	        "legendshadow": "0",
+	        "legendborderalpha": "0",
+	        "canvasborderalpha": "50",
+	        "numvdivlines": "5",
+	        "vdivlinealpha": "20",
+	        "showborder": "0"
+	    },
+	    "categories": [
+	        {
+	            "category": [
+	                {
+	                    "label": "8/6/2006"
+	                },
+	                {
+	                    "label": "8/7/2006"
+	                },
+	                {
+	                    "label": "8/8/2006"
+	                },
+	                {
+	                    "label": "8/9/2006"
+	                },
+	                {
+	                    "label": "8/10/2006"
+	                },
+	                {
+	                    "label": "8/11/2006"
+	                },
+	                {
+	                    "label": "8/12/2006"
+	                }
+	            ]
+	        }
+	    ],
+	    "dataset": [
+	        {
+	            "seriesname": "Offline Marketing",
+	            "color": "1D8BD1",
+	            "anchorbordercolor": "1D8BD1",
+	            "anchorbgcolor": "1D8BD1",
+	            "data": [
+	                {
+	                    "value": "1327"
+	                },
+	                {
+	                    "value": "1826"
+	                },
+	                {
+	                    "value": "1699"
+	                },
+	                {
+	                    "value": "1511"
+	                },
+	                {
+	                    "value": "1904"
+	                },
+	                {
+	                    "value": "1957"
+	                },
+	                {
+	                    "value": "1296"
+	                }
+	            ]
+	        },
+	        {
+	            "seriesname": "Search",
+	            "color": "F1683C",
+	            "anchorbordercolor": "F1683C",
+	            "anchorbgcolor": "F1683C",
+	            "data": [
+	                {
+	                    "value": "2042"
+	                },
+	                {
+	                    "value": "3210"
+	                },
+	                {
+	                    "value": "2994"
+	                },
+	                {
+	                    "value": "3115"
+	                },
+	                {
+	                    "value": "2844"
+	                },
+	                {
+	                    "value": "3576",
+	                    "displayvalue": "New listing in Yahoo & Bing",
+	                    "showvalue": "1"
+	                },
+	                {
+	                    "value": "1862"
+	                }
+	            ]
+	        },
+	        {
+	            "seriesname": "Paid Search",
+	            "color": "2AD62A",
+	            "anchorbordercolor": "2AD62A",
+	            "anchorbgcolor": "2AD62A",
+	            "data": [
+	                {
+	                    "value": "850"
+	                },
+	                {
+	                    "value": "1010"
+	                },
+	                {
+	                    "value": "1116"
+	                },
+	                {
+	                    "value": "1234"
+	                },
+	                {
+	                    "value": "1210"
+	                },
+	                {
+	                    "value": "1054"
+	                },
+	                {
+	                    "value": "802"
+	                }
+	            ]
+	        },
+	        {
+	            "seriesname": "From Mail",
+	            "color": "DBDC25",
+	            "anchorbordercolor": "DBDC25",
+	            "anchorbgcolor": "DBDC25",
+	            "data": [
+	                {
+	                    "value": "541"
+	                },
+	                {
+	                    "value": "781"
+	                },
+	                {
+	                    "value": "920"
+	                },
+	                {
+	                    "value": "754"
+	                },
+	                {
+	                    "value": "840"
+	                },
+	                {
+	                    "value": "893"
+	                },
+	                {
+	                    "value": "451"
+	                }
+	            ]
+	        }
+	    ]
+	      }
+	 
+	  });
+	  revenueChart.render("chartContainer");
+	}); 
 
    function toggleGraph(){
    	$('#chartdiv').toggle('show');
