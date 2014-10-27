0249f93713f4070ac830b5203d2bedc533be7bf3
Thomas Mapesa <mapesa@gmail.com>
2014-10-27 15:42:35 +0300

    Localization for SpecimenType views.

--- app/views/specimentype/create.blade.php
+++ app/views/specimentype/create.blade.php
@@ -2,17 +2,17 @@
 @section("content")
 <div>
 	<ol class="breadcrumb">
-	  <li><a href="{{{URL::route('user.home')}}}">Home</a></li>
+	  <li><a href="{{{URL::route('user.home')}}}">{{trans('messages.home')}}</a></li>
 	  <li>
-	  	<a href="{{ URL::route('specimentype.index') }}">Specimen Type</a>
+	  	<a href="{{ URL::route('specimentype.index') }}">{{Lang::choice('messages.specimen-type',2)}}</a>
 	  </li>
-	  <li class="active">Create Specimen Type</li>
+	  <li class="active">{{trans('messages.create-specimen-type')}}</li>
 	</ol>
 </div>
 <div class="panel panel-primary">
 	<div class="panel-heading ">
 		<span class="glyphicon glyphicon-user"></span>
-		Create Specimen Type
+		{{trans('messages.create-specimen-type')}}
 	</div>
 	<div class="panel-body">
 	<!-- if there are creation errors, they will show here -->
@@ -26,17 +26,17 @@
 		{{ Form::open(array('url' => 'specimentype', 'id' => 'form-create-specimentype')) }}
 
 			<div class="form-group">
-				{{ Form::label('name', 'Name') }}
+				{{ Form::label('name', Lang::choice('messages.name', 1)) }}
 				{{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
 			</div>
 			<div class="form-group">
-				{{ Form::label('description', 'Description') }}
+				{{ Form::label('description', trans('messages.description')) }}
 				{{ Form::textarea('description', Input::old('description'), 
 					array('class' => 'form-control', 'rows' => '2')) }}
 			</div>
 			<div class="form-group actions-row">
 				{{ Form::button(
-					'<span class="glyphicon glyphicon-save"></span> Save',
+					'<span class="glyphicon glyphicon-save"></span> '.trans('messages.save'),
 					['class' => 'btn btn-primary', 'onclick' => 'submit()'] 
 				) }}
 			</div>
