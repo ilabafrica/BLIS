a9299b575099ba0698ec5e675a70c19ee6699375
Kitsao Emmanuel <kittemm@gmail.com>
2014-11-27 13:56:16 +0300

    removed tables in filters, localized select boxes

--- app/views/reports/patient/report.blade.php
+++ app/views/reports/patient/report.blade.php
@@ -7,37 +7,51 @@
 	  <li class="active">{{ trans('messages.patient-report') }}</li>
 	</ol>
 </div>
-{{ Form::open(array('url' => 'patientreport/'.$patient->id, 'id' => 'form-patientreport-filter', 'method'=>'POST')) }}
-	{{ Form::hidden('patient', $patient->id, array('id' => 'patient')) }}
-	<div class="table-responsive">
-	  <table class="table report-filter">
-	    <tbody>
-	    <tr>
-	    	<td>
-	        	<label class="checkbox-inline">
-	        		{{ Form::checkbox('pending', "1", isset($pending)) }}
-	              	{{trans('messages.include-pending-tests')}}
+<div class='container-fluid'>
+    {{ Form::open(array('url' => 'patientreport/'.$patient->id, 'class' => 'form-inline', 'id' => 'form-patientreport-filter', 'method'=>'POST')) }}
+		{{ Form::hidden('patient', $patient->id, array('id' => 'patient')) }}
+		<div class="row">
+			<div class="col-sm-3">
+				<label class="checkbox-inline">
+	        		{{ Form::checkbox('pending', "1", isset($pending)) }}{{trans('messages.include-pending-tests')}}
 				</label>
-	        </td>
-	        <td>{{ Form::label('start', trans("messages.from")) }}</td>
-        	<td>
-                {{ Form::text('start', isset($input['start'])?$input['start']:date('Y-m-d'), 
-                    array('class' => 'form-control standard-datepicker')) }}
-                </td>
-	        <td>{{ Form::label('end', trans("messages.to")) }}</td>
-        	<td>
-                {{ Form::text('end', isset($input['end'])?$input['end']:date('Y-m-d'), 
-                    array('class' => 'form-control standard-datepicker')) }}
-                </td>
-	        <td>{{ Form::button("<span class='glyphicon glyphicon-filter'></span> ".trans('messages.view'), 
-                    array('class' => 'btn btn-primary', 'id' => 'filter', 'type' => 'submit')) }}</td>
-	        <td>{{ Form::submit(trans('messages.export-to-word'), array('class' => 'btn btn-success', 
-	        	'id' => 'word', 'name' => 'word')) }}</td>
-	    </tr>
-		</tbody>
-	</table>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-2">
+						{{ Form::label('start', trans("messages.from")) }}</div><div class="col-sm-1">
+			        	{{ Form::text('start', isset($input['start'])?$input['start']:date('Y-m-d'), 
+			                array('class' => 'form-control standard-datepicker')) }}
+			        </div>
+		        </div>
+	        </div>
+	        <div class="col-sm-3">
+				<div class="row">
+			        <div class="col-sm-2">
+				        {{ Form::label('end', trans("messages.to")) }}
+				    </div>
+				    <div class="col-sm-1">
+		                {{ Form::text('end', isset($input['end'])?$input['end']:date('Y-m-d'), 
+		                    array('class' => 'form-control standard-datepicker')) }}
+		            </div>
+	            </div>
+            </div>
+            <div class="col-sm-3">
+				<div class="row">
+		            <div class="col-sm-4">
+			            {{ Form::button("<span class='glyphicon glyphicon-filter'></span> ".trans('messages.view'), 
+			                    array('class' => 'btn btn-primary', 'id' => 'filter', 'type' => 'submit')) }}
+		            </div>
+		            <div class="col-sm-1">
+				        {{ Form::submit(trans('messages.export-to-word'), array('class' => 'btn btn-success', 
+				        	'id' => 'word', 'name' => 'word')) }}
+				    </div>
+			    </div>
+		    </div>
+	    </div>
+	{{ Form::close() }}
 </div>
-{{ Form::close() }}
+<br />
 <div class="panel panel-primary" id="patientReport">
 	<div class="panel-heading ">
 		<span class="glyphicon glyphicon-user"></span>
