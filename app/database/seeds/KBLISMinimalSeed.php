10a6176f9e246a6aa3bd7f273f041a5b7180663c
Thomas Mapesa <mapesa@gmail.com>
2015-01-13 16:15:35 +0300

    Seed file with the minimal data required to launch BLIS.

---
+++ app/database/seeds/KBLISMinimalSeed.php
@@ -0,0 +1,119 @@
+<?php
+
+class KBLISMinimalSeed extends Seeder
+{
+    public function run()
+    {
+        Eloquent::unguard();
+
+        /* Users table */
+        $usersData = array(
+            array(
+                "username" => "administrator", "password" => Hash::make("password"), "email" => "admin@kblis.org",
+                "name" => "kBLIS Administrator", "designation" => "Administrator"
+            ),
+            array(
+                "username" => "external", "password" => Hash::make("password"), "email" => "admin@kblis.org",
+                "name" => "External System User", "designation" => "Administrator", "image" => "/i/users/user-2.jpg"
+            ),
+        );
+
+        foreach ($usersData as $user)
+        {
+            $users[] = User::create($user);
+        }
+        $this->command->info('users seeded');
+        
+        /* Measure Types */
+        $measureTypes = array(
+            array("id" => "1", "name" => "Numeric Range"),
+            array("id" => "2", "name" => "Alphanumeric Values"),
+            array("id" => "3", "name" => "Autocomplete"),
+            array("id" => "4", "name" => "Free Text")
+        );
+
+        foreach ($measureTypes as $measureType)
+        {
+            MeasureType::create($measureType);
+        }
+        $this->command->info('measure_types seeded');
+                
+        /* Test Phase table */
+        $test_phases = array(
+          array("id" => "1", "name" => "Pre-Analytical"),
+          array("id" => "2", "name" => "Analytical"),
+          array("id" => "3", "name" => "Post-Analytical")
+        );
+        foreach ($test_phases as $test_phase)
+        {
+            TestPhase::create($test_phase);
+        }
+        $this->command->info('test_phases seeded');
+
+        /* Test Status table */
+        $test_statuses = array(
+          array("id" => "1","name" => "not-received","test_phase_id" => "1"),//Pre-Analytical
+          array("id" => "2","name" => "pending","test_phase_id" => "1"),//Pre-Analytical
+          array("id" => "3","name" => "started","test_phase_id" => "2"),//Analytical
+          array("id" => "4","name" => "completed","test_phase_id" => "3"),//Post-Analytical
+          array("id" => "5","name" => "verified","test_phase_id" => "3")//Post-Analytical
+        );
+        foreach ($test_statuses as $test_status)
+        {
+            TestStatus::create($test_status);
+        }
+        $this->command->info('test_statuses seeded');
+
+        /* Specimen Status table */
+        $specimen_statuses = array(
+          array("id" => "1", "name" => "specimen-not-collected"),
+          array("id" => "2", "name" => "specimen-accepted"),
+          array("id" => "3", "name" => "specimen-rejected")
+        );
+        foreach ($specimen_statuses as $specimen_status)
+        {
+            SpecimenStatus::create($specimen_status);
+        }
+        $this->command->info('specimen_statuses seeded');
+
+        /* Permissions table */
+        $permissions = array(
+            array("name" => "view_names", "display_name" => "Can view patient names"),
+            array("name" => "manage_patients", "display_name" => "Can add patients"),
+
+            array("name" => "receive_external_test", "display_name" => "Can receive test requests"),
+            array("name" => "request_test", "display_name" => "Can request new test"),
+            array("name" => "accept_test_specimen", "display_name" => "Can accept test specimen"),
+            array("name" => "reject_test_specimen", "display_name" => "Can reject test specimen"),
+            array("name" => "change_test_specimen", "display_name" => "Can change test specimen"),
+            array("name" => "start_test", "display_name" => "Can start tests"),
+            array("name" => "enter_test_results", "display_name" => "Can enter tests results"),
+            array("name" => "edit_test_results", "display_name" => "Can edit test results"),
+            array("name" => "verify_test_results", "display_name" => "Can verify test results"),
+            array("name" => "send_results_to_external_system", "display_name" => "Can send test results to external systems"),
+            array("name" => "refer_specimens", "display_name" => "Can refer specimens"),
+
+            array("name" => "manage_users", "display_name" => "Can manage users"),
+            array("name" => "manage_test_catalog", "display_name" => "Can manage test catalog"),
+            array("name" => "manage_lab_configurations", "display_name" => "Can manage lab configurations"),
+            array("name" => "view_reports", "display_name" => "Can view reports")
+        );
+        foreach ($permissions as $permission) {
+            Permission::create($permission);
+        }
+        $this->command->info('Permissions table seeded');
+
+        /* Roles table */
+        $roles = array(
+            array("name" => "Superadmin"),
+            array("name" => "Technologist"),
+            array("name" => "Receptionist")
+        );
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
+        $this->command->info('Roles table seeded');
+
+    }
+
+}
