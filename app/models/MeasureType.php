902f5a83d91894a02ba1a126ff682ad615114b54
Thomas Mapesa <mapesa@gmail.com>
2015-01-19 11:47:10 +0300

    Added TestType filter to prevalence report.

--- app/models/MeasureType.php
+++ app/models/MeasureType.php
@@ -11,58 +11,11 @@ class MeasureType extends Eloquent
 	protected $table = 'measure_types';
 
 	/**
-	 *  Check to if the Measure Type is Numeric
-	 *
-	 * @return boolean
+	 * Measure relationship
 	 */
-	public function isNumeric()
-	{
-		if($this->id == 1){
-			return true;
-		}
-		else 
-			return false;
-	}
+    public function measures()
+    {
+        return $this->hasMany('Measure');
+    }
 
-	/**
-	 *  Check to if the Measure Type is Alphanumeric
-	 *
-	 * @return boolean
-	 */
-	public function isAlphanumeric()
-	{
-		if($this->id == 2){
-			return true;
-		}
-		else 
-			return false;
-	}
-
-	/**
-	 *  Check to if the Measure Type is Autocomplete
-	 *
-	 * @return boolean
-	 */
-	public function isAutocomplete()
-	{
-		if($this->id == 3){
-			return true;
-		}
-		else 
-			return false;
-	}
-
-	/**
-	 *  Check to if the Measure Type is Free Text
-	 *
-	 * @return boolean
-	 */
-	public function isFreeText()
-	{
-		if($this->id == 4){
-			return true;
-		}
-		else 
-			return false;
-	}
 }
