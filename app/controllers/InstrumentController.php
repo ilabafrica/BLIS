1b76a89ef5ec6d0a20f4088053af1d7b03693a69
Brian Maiyo <kipropbrian@gmail.com>
2015-01-26 20:49:27 +0300

    Cleanup, changing lab_request_columns

--- app/controllers/InstrumentController.php
+++ app/controllers/InstrumentController.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\QueryException;
-// use KBLIS\Instrumentation\;
 
 /**
  *Contains functions for managing instruments
@@ -15,108 +14,13 @@ class InstrumentController extends \BaseController {
 	 * @return Response
 	 */
 	public function index()
-	{		
-			$i = 0;
-			$count = 20000;
+	{
+		// List all the active instruments
+			$instruments = Instrument::paginate(Config::get('kblis.page-items'));
 
-			while($i < $count){
-				$dumps = ExternalDump::whereRaw("id between $i +1 and $i + 1000")->get();
-
-				foreach ($dumps as $key => $dump) {
-					$this->process($dump);
-				}
-				$i+=1000;
-			}
-
-		// // List all the active instruments
-		// $instruments = Instrument::paginate(Config::get('kblis.page-items'));
-
-		// // Load the view and pass the instruments
-		// return View::make('instrument.index')->with('instruments', $instruments);
+		// Load the view and pass the instruments
+		return View::make('instrument.index')->with('instruments', $instruments);
 	}
-
-	public function process($labRequest)
-    {
-    	if($labRequest->patient_visit_number == null || $labRequest->result_returned == 1) 
-        {
-        	return;
-        }
-        //First: Check if patient exists, if true dont save again
-        $patient = Patient::where('patient_number', '=', $labRequest->patient_id)->get();
-        
-        if ($patient->isEmpty())
-        {
-            $patient = new Patient();
-            $patient->patient_number = $labRequest->patient_id;
-            $patient->name = $labRequest->full_name;
-            $gender = array('Male' => Patient::MALE, 'Female' => Patient::FEMALE); 
-            $patient->gender = $gender[$labRequest->gender];
-            $patient->dob = $labRequest->dob;
-            $patient->address = $labRequest->address;
-            $patient->phone_number = $labRequest->phone_number;
-            $patient->save();
-        }
-
-        //We check if the test exists in our system if not we just save the request in stagingTable
-        if($labRequest->parent_lab_no == '0')
-        {
-            $testTypeId = TestType::getTestTypeIdByTestName($labRequest->investigation);
-        }
-        else {
-            $testTypeId = null;
-        }
-
-        if(is_null($testTypeId) && $labRequest->parent_lab_no == '0')
-        {
-            return;
-        }
-
-        //Check if visit exists, if true dont save again
-        $visitCheck = Visit::where('visit_number', '=', $labRequest->patient_visit_number)->get();
-
-        if ($visitCheck->isEmpty())
-        {
-            $visit = new Visit();
-            $visit->patient_id = $patient->id;
-            $visitType = array('ip' => 'In-patient', 'op' => 'Out-patient');//Should be a constant
-            $visit->visit_type = $visitType[$labRequest->order_stage];
-            $visit->visit_number = $labRequest->patient_visit_number;
-
-            // We'll save Visit in a transaction a little bit below
-        }
-
-        $test = null;
-        //Check if parent_lab_nO is 0 thus its the main test and not a measure
-        if($labRequest->parent_lab_no == '0')
-        {
-            //Check via the labno, if this is a duplicate request and we already saved the test 
-            $test = Test::where('external_id', '=', $labRequest->lab_no)->get();
-            if ($test->isEmpty())
-            {
-                //Specimen
-                $specimen = new Specimen();
-                $specimen->specimen_type_id = TestType::find($testTypeId)->specimenTypes->lists('id')[0];
-
-                // We'll save the Specimen in a transaction a little bit below
-
-                $test = new Test();
-                $test->test_type_id = $testTypeId;
-                $test->test_status_id = Test::NOT_RECEIVED;
-                $test->created_by = User::EXTERNAL_SYSTEM_USER; //Created by external system 0
-                $test->requested_by = $labRequest->requesting_clinician;
-                $test->external_id = $labRequest->lab_no;
-                $test->time_created = $labRequest->request_date;
-
-                DB::transaction(function() use ($visit, $specimen, $test) {
-                    $visit->save();
-                    $specimen->save();
-                    $test->visit_id = $visit->id;
-                    $test->specimen_id = $specimen->id;
-                    $test->save();
-                });
-            }
-        }
-    }
 
 	/**
 	 * Show the form for creating a new instrument.
