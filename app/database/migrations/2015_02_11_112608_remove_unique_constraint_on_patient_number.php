6bf8c7e87f0b87acb312f471aa1cd9cbc9111e8a
Brian Maiyo <kipropbrian@gmail.com>
2015-02-11 11:27:45 +0300

    migration to remove uniqueness of patient_number

---
+++ app/database/migrations/2015_02_11_112608_remove_unique_constraint_on_patient_number.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveUniqueConstraintOnPatientNumber extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('patients', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('patients', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
