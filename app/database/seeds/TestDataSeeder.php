2a845572ce7df67d55c88876dc54063737fc0777
Kitsao Emmanuel <kittemm@gmail.com>
2015-03-18 22:31:56 +0300

    separate seed for microbiology C/S

--- app/database/seeds/TestDataSeeder.php
+++ app/database/seeds/TestDataSeeder.php
@@ -1482,37 +1482,6 @@ class TestDataSeeder extends DatabaseSeeder
             ReportDisease::create($reportDisease);
         }
         $this->command->info("Report Disease table seeded");
-
-        //Seed for drugs
-        $drugs = array(
-            array('name' => "PENICILLIN"),
-            array('name' => "AMPICILLIN"),
-            array('name' => "CLINDAMYCIN"),
-            array('name' => "TETRACYCLINE"),
-            array('name' => "CIPROFLOXACIN"),
-            array('name' => "TRIMETHOPRIM/SULFA"),
-            array('name' => "NITROFURANTOIN"),
-            array('name' => "CHLORAMPHENICOL"),
-            array('name' => "CEFAZOLIN"),
-            array('name' => "GANTAMICIN"),
-            array('name' => "AMOXICILLIN-CLAV"),
-            array('name' => "CEPHALOTHIN"),
-            array('name' => "CEFUROXIME"),
-            array('name' => "CEFOTAXIME"),
-            array('name' => "PIPERACILLIN"),
-            array('name' => "CEFIXIME"),
-            array('name' => "CEFTAZIDIME"),
-            array('name' => "CEFRIAXONE"),
-            array('name' => "LEVOFLOXACIN"),
-            array('name' => "MERODENEM"),
-            array('name' => "PIPERACILLIN/TAZO"),
-            array('name' => "IMEDENEM")
-        );
-
-        foreach ($drugs as $drug) {
-            $addedDrugs[] = Drug::create($drug);
-        }
-        $this->command->info('Drugs table seeded');
     }
 
     public function createSpecimen(
