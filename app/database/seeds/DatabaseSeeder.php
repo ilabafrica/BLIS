5376d269396dd5dec805ebac3b50949a17e7aca0
Brian Maiyo <kipropbrian@gmail.com>
2015-03-18 22:43:57 +0300

    fix for seeding

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 		// $this->call('KBLISSeeder');
-		$this->call('InventoryQCPermissionsSeeder');
+		$this->call('TestDataSeeder');
 	}
 
 }
