f872123a93d76e47a194c0510b7d32d5f91660a9
Kitsao Emmanuel <kittemm@gmail.com>
2016-04-06 11:16:14 +0300

    corrected failing travis

--- app/controllers/BarcodeController.php
+++ app/controllers/BarcodeController.php
@@ -9,11 +9,17 @@ class BarcodeController extends \BaseController {
 	 */
 	public function index()
 	{
+
+	// Constants for use
+		$format = ['ean8', 'ean13', 'code11', 'code39', 'code128', 'codabar', 'std25', 'int25', 'code93'];
+		$width = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+		$height = ['5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80'];
+		$font = ['5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39'];
 		//	Get variables for use
-		$encoding_format = self::keyval(Barcode::ENCODING_FORMAT);
-		$barcode_width = self::keyval(Barcode::BARCODE_WIDTH);
-		$barcode_height = self::keyval(Barcode::BARCODE_HEIGHT);
-		$text_size = self::keyval(Barcode::TEXT_SIZE);
+		$encoding_format = self::keyval($format);
+		$barcode_width = self::keyval($width);
+		$barcode_height = self::keyval($height);
+		$text_size = self::keyval($font);
 		//	Get already set barcode
 		$barcode = Barcode::first();
 		return View::make('barcode.index')->with('encoding_format', $encoding_format)->with('barcode_width', $barcode_width)->with('barcode_height', $barcode_height)->with('text_size', $text_size)->with('barcode', $barcode);	
