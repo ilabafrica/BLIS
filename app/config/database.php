9806d145a187ee8e049fb86842087ab8396b5b7b
ereng <aereng@gmail.com>
2016-04-14 14:00:41 +0300

    put things back

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'blis25',
+			'database'  => 'iblis',
 			'username'  => 'root',
-			'password'  => '1234',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
