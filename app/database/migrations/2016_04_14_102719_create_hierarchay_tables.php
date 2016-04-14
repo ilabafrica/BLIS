53a433d747d24ca66aa060305abba15857fb5588
Piusyo <pmpiusyo@gmail.com>
2016-04-14 11:18:28 +0300

    Creating-regiontypes

--- app/database/migrations/2016_04_14_102719_create_hierarchay_tables.php
+++ app/database/migrations/2016_04_14_102719_create_hierarchay_tables.php
@@ -13,6 +13,56 @@ class CreateHierarchayTables extends Migration {
 	public function up()
 	{
 		//
+		Schema::create('region_types', function(Blueprint $table)
+        {
+            $table->increments('id')->unsigned();
+            $table->string('name',100)->unique();
+            $table->integer('user_id')->unsigned();
+
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+		//Counties tables
+		Schema::create('regions', function(Blueprint $table)
+        {
+            $table->increments('id')->unsigned();
+            $table->string('name',100)->unique();
+            $table->integer('region_type_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+
+            $table->foreign('region_type_id')->references('id')->on('region_types');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('region_tiers', function(Blueprint $table)
+        {
+            $table->increments('id')->unsigned();
+            $table->integer('region_id')->unsigned();
+            $table->integer('tier_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+
+            $table->foreign('region_id')->references('id')->on('regions');
+            $table->foreign('tier_id')->references('id')->on('regions');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+       
+
+        Schema::table('facilities', function(Blueprint $table)
+		{
+			$table->integer('region_id')->unsigned()->after('name');
+
+			$table->foreign('region_id')->references('id')->on('regions');
+		});
 	}
 
 	/**
@@ -22,7 +72,14 @@ class CreateHierarchayTables extends Migration {
 	 */
 	public function down()
 	{
-		//
+		
+		Schema::table('facilities', function(Blueprint $table){
+			$table->dropColumn('region_id');
+		});
+		Schema::dropIfExists('region_types');
+		Schema::dropIfExists('regions');
+		Schema::dropIfExists('region-tiers');
 	}
 
 }
+
---
+++ app/views/regiontype/create.blade.php
@@ -0,0 +1,46 @@
+@extends("layout")
+@section("content")
+<br />
+<div>
+  <ol class="breadcrumb">
+    <li><a href="{{{URL::route('user.home')}}}">{{ trans('messages.home') }}</a></li>
+    <li class="active">{{Lang::choice('messages.region-type',2)}}</li>
+  </ol>
+</div>
+<div class="panel panel-primary">
+    <div class="panel-heading"><i class="fa fa-tags"></i> {{ Lang::choice('messages.create-region-type', '2') }}</div>
+    <div class="panel-body">
+        <div class="col-lg-6 main">
+            <!-- Begin form --> 
+            @if($errors->all())
+            <div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                {{ HTML::ul($errors->all(), array('class'=>'list-unstyled')) }}
+            </div>
+            @endif
+            {{ Form::open(array('route' => 'regiontype.store', 'id' => 'form-add-region', 'class' => 'form-horizontal')) }}
+                <!-- CSRF Token -->
+                <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+                <!-- ./ csrf token -->
+                <div class="form-group">
+                    {{ Form::label('name', Lang::choice('messages.name', 1), array('class' => 'col-sm-4 control-label')) }}
+                    <div class="col-sm-8">
+                        {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-8">
+                    {{ Form::button("<i class='glyphicon glyphicon-ok-circle'></i> ".Lang::choice('messages.save', 1), 
+                          array('class' => 'btn btn-success', 'onclick' => 'submit()')) }}
+                          {{ Form::button("<i class='glyphicon glyphicon-remove-circle'></i> ".'Reset', 
+                          array('class' => 'btn btn-default', 'onclick' => 'reset()')) }}
+                    <a href="#" class="btn btn-s-md btn-warning"><i class="glyphicon glyphicon-ban-circle"></i> {{ Lang::choice('messages.cancel', 1) }}</a>
+                    </div>
+                </div>
+            {{ Form::close() }} 
+            <!-- End form -->
+        </div> 
+    </div>
+</div>
+@stop
