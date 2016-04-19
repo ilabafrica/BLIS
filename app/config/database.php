1ec6dc51224c8283dadfe7899ca456eda35cfb2d
Kitsao Emmanuel <kittemm@gmail.com>
2016-04-19 19:58:46 +0300

    cleared db password

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'iblis',
 			'username'  => 'root',
-			'password'  => 'xp',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
