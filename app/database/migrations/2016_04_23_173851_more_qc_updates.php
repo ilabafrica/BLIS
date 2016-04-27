b75056fd6de0aa6413995ac6ae317a775cce9528
Kitsao Emmanuel <kittemm@gmail.com>
2016-04-27 13:58:55 +0300

    defaults

--- app/database/migrations/2016_04_23_173851_more_qc_updates.php
+++ app/database/migrations/2016_04_23_173851_more_qc_updates.php
@@ -22,17 +22,17 @@ class MoreQcUpdates extends Migration {
 			$table->dropForeign('control_tests_entered_by_foreign');
 			$table->dropColumn('entered_by');
 
-			$table->integer('lot_id')->unsigned()->after('control_id');
+			$table->integer('lot_id')->unsigned()->default(0)->after('control_id');
 			$table->foreign('lot_id')->references('id')->on('lots');
 
 			$table->string('performed_by', 100)->after('lot_id');
 			
-			$table->integer('user_id')->unsigned()->after('lot_id');
+			$table->integer('user_id')->unsigned()->default(1)->after('lot_id');
 			$table->foreign('user_id')->references('id')->on('users');
 		});
 		Schema::table('control_results', function(Blueprint $table)
 		{
-			$table->integer('user_id')->unsigned()->after('control_test_id');
+			$table->integer('user_id')->unsigned()->default(1)->after('control_test_id');
 			$table->foreign('user_id')->references('id')->on('users');
 		});
 	}
