aaa17ba880f82ae5d62190b8c140e9957c95c3aa
kitsao <kittemm@gmail.com>
2016-05-12 16:57:35 +0300

    uncommented seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,8 +10,8 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-		// $this->call('KBLISSeeder');
-		// $this->call('TestDataSeeder');
+		$this->call('KBLISSeeder');
+		$this->call('TestDataSeeder');
 		$this->call('ConfigSettingSeeder');
 		// $this->call('CCCSeeder');
 	}
