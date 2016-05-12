6882be9d8df14292c224532bd725d0d990ce8d61
Piusyo <pmpiusyo@gmail.com>
2016-05-12 18:13:29 +0300

    Hierarchy seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-		$this->call('KBLISSeeder');
+		// $this->call('KBLISSeeder');
 		$this->call('TestDataSeeder');
 		$this->call('ConfigSettingSeeder');
 		// $this->call('CCCSeeder');
