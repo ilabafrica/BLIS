ec9c627a444d45547334beb5450abec04f3debe9
Derrick Rono <drono@strathmore.edu>
2017-06-14 13:35:01 +0300

    solved failing tests in measure controller

--- app/tests/MeasureControllerTest.php
+++ app/tests/MeasureControllerTest.php
@@ -260,7 +260,7 @@ class MeasureControllerTest extends TestCase
 				'name' => 'Numeric',
 				'measure_type_id' => '1',
 				'unit' => 'nUnit',
-				'interval'=>['0', '0'],
+				'interval'=>['1', '1'],
 				'description' => 'nDescription',
 				'agemin' =>['11', '21'], 
 				'agemax' => ['41', '51'], 
