d476dcca9fb55b962447535a9184428063d030ce
ereng <aereng@gmail.com>
2019-02-21 16:04:10 +0300

    yet another slight navigation fix

--- app/views/mh4lmapper/create.blade.php
+++ app/views/mh4lmapper/create.blade.php
@@ -3,7 +3,7 @@
 	<div>
 		<ol class="breadcrumb">
 		  <li><a href="{{{URL::route('user.home')}}}">{{ trans('messages.home') }}</a></li>
-		  <li><a href="{{ URL::route('patient.index') }}">{{ Lang::choice('mH4lmapper',2) }}</a></li>
+		  <li><a href="{{ URL::route('mh4lmapper.index') }}">{{ Lang::choice('mH4lmapper',2) }}</a></li>
 		  <li class="active">{{trans('create mH4lmapping')}}</li>
 		</ol>
 	</div>
